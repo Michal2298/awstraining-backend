b0228deb0e13f65cd2a59a9d9664ab17e9c3857e
Michał Kloc <michal.kloc@capgemini.com>
2024-09-05 14:57:39 +0200

    Implementation of Translator

--- app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
@@ -10,11 +10,13 @@ public class NotifyMeService {
     private MessageSender sender;
     // TODO lab2
     //  1. Inject Translator
+    private Translator translator;
     // TODO lab3
     //  1. Inject sentiment detector
 //    @Autowired
-    public NotifyMeService(MessageSender sender) {
+    public NotifyMeService(MessageSender sender, Translator translator) {
         this.sender =sender;
+        this.translator = translator;
 
     }
     
@@ -23,14 +25,18 @@ public class NotifyMeService {
         // TODO: lab1
         //  1. Send text using sender.
         //  2. Return sent message.
-        sender.send(notifyMe.text());
+//        sender.send(notifyMe.text());
+//        return notifyMe.text();
         // TODO: lab2
         //  1. Translate text from using translator.
         //  2. Change sending of text to "translated text" and return it.
+        String translatedText = translator.translate(notifyMe);
+        sender.send(translatedText);
+        return translatedText;
         // TODO: lab3
         //  1. Detect sentiment of translated message.
         //  2. Change sending of text to "setiment: translated text" and return it.
-        return notifyMe.text();
+
     }
     
 }
