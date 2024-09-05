6cc9396cfcacd9a9a0685cbdb4ab3dbff80aa4dd
Michał Kloc <michal.kloc@capgemini.com>
2024-09-05 14:09:44 +0200

    Implementation 1

--- app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
@@ -7,12 +7,14 @@ public class NotifyMeService {
 
     // TODO: lab1
     //  1. Inject MessageSender.
+    private MessageSender sender;
     // TODO lab2
     //  1. Inject Translator
     // TODO lab3
     //  1. Inject sentiment detector
 //    @Autowired
-    public NotifyMeService() {
+    public NotifyMeService(MessageSender sender) {
+        this.sender =sender;
 
     }
     
@@ -21,13 +23,14 @@ public class NotifyMeService {
         // TODO: lab1
         //  1. Send text using sender.
         //  2. Return sent message.
+        sender.send(notifyMe.text());
         // TODO: lab2
         //  1. Translate text from using translator.
         //  2. Change sending of text to "translated text" and return it.
         // TODO: lab3
         //  1. Detect sentiment of translated message.
         //  2. Change sending of text to "setiment: translated text" and return it.
-        return "This service is not available.";
+        return notifyMe.text();
     }
     
 }
